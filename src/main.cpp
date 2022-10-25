4b6114705037dac655b46b35614838c1b24652ee
Thierry Raeber <cyphics@protonmail.com>
2022-10-25 12:06:11 +0200

    famous gradient triangle!

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 
 const char *vertexShaderSource = "#version 330 core\n"
-                                  "layout (location = 0) in vec3 aPos;\n"
-                                  "out vec4 vertexColor;\n"
-                                  "void main()\n"
-                                  "{\n"
-                                  " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                  " vertexColor = vec4(0.4, 0.0, 0.0, 1.0);\n"
-                                  "}\0";
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "out vec3 ourColor;\n"
+                                 "void main() {\n"
+                                 " gl_Position = vec4(aPos, 1.0);\n"
+                                 " ourColor = aColor;\n"
+                                 "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
                                   "out vec4 FragColor;\n"
-//                                  "in vec4 vertexColor;\n"
-                                  "uniform vec4 ourColor;\n"
+                                  "in vec3 ourColor;\n"
                                   "void main()\n"
                                   "{\n"
-                                  " FragColor = ourColor;\n"
+                                  " FragColor = vec4(ourColor, 1.0);\n"
                                   "}\0";
 
 
@@ -89,9 +88,9 @@ int main() {
     // set up vertex data (and buffers) and configure vertex attributes
     // add a new set of vcertices to form a second triangle (total of 6 vertices); the vertex attribute config remains the same
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.0f, 0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+            -0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
     };
 
     unsigned int VAO, VBO;
@@ -104,8 +103,10 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) , vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // unbind (optional)
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -120,13 +121,6 @@ int main() {
 
         glUseProgram(shaderProgram);
 
-        auto timeValue = (float) glfwGetTime();
-        auto greenValue = (float)(sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        if (vertexColorLocation == -1) {
-            std::cout << "Unable to find uniform 'ourColor'" << std::endl;
-        }
-        glUniform4f(vertexColorLocation, greenValue, greenValue, greenValue, 1.0f);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 //        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
