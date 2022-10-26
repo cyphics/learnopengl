7d0eb5a6f2216f598e3d6e50e10bbf8e9fa466b3
Thierry Raeber <cyphics@protonmail.com>
2022-10-26 14:22:34 +0200

    First rotating cube!

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
     // glfw window creation
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
@@ -41,18 +42,64 @@ int main() {
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     // build and compile our shader program
     Shader ourShader("../shaders/vertexShader.shader", "../shaders/fragmentShader.shader");
 
     // set up vertex data (and buffers) and configure vertex attributes
     // add a new set of vcertices to form a second triangle (total of 6 vertices); the vertex attribute config remains the same
     float vertices[] = {
-            // positions          // colors           // texture coords
-             0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-             0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+             0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f,  1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f
     };
+//    float vertices[] = {
+//            // positions          // colors           // texture coords
+//             0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+//             0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+//            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+//            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+//    };
+
     unsigned int indices[] = {
             0, 1, 3,
             1, 2, 3
@@ -73,13 +120,13 @@ int main() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
+//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
+//    glEnableVertexAttribArray(1);
     // texture attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(2);
 
     unsigned int texture1, texture2;
@@ -124,19 +171,12 @@ int main() {
     glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); // manually
     ourShader.setInt("texture2", 1); // or with shader class
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.f),
-                        glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
         glActiveTexture(GL_TEXTURE0);
@@ -145,6 +185,14 @@ int main() {
         glBindTexture(GL_TEXTURE_2D, texture2);
         glBindVertexArray(VAO);
 
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f),
+                            glm::vec3(0.5f, 1.0f, 0.0f));
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
@@ -152,7 +200,8 @@ int main() {
         unsigned int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
